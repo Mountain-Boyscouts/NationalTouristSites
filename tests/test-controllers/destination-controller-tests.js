6c968cfda47f58824181ad9fa4d0df3ccd0ced02
Alex Nestorov <nestorov.alexander@gmail.com>
2017-05-17 16:59:57 +0300

    return ecma script6

--- tests/test-controllers/destination-controller-tests.js
+++ tests/test-controllers/destination-controller-tests.js
@@ -7,7 +7,7 @@ const data = {};
 
 const controller = require("../../app/controllers/destination-controller")(data);
 
-describe("NationalTouristSites/app/controllers/destination-controller.js tests", function(){
+describe("NationalTouristSites/app/controllers/destination-controller.js tests", ()=>{
     it("Expect addComment to exist and be a function.", () => {
             expect(controller.addComment).to.exist;
             expect(controller.addComment).to.be.a("function");
